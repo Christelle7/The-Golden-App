7efc7a2d783f2d1c1c09a44724048f7da9f21dfd
Christelle <hopeanddash@gmail.com>
2019-12-11 19:31:43 -0600

    voila

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
-//const config = require('config');
+const config = require('config');
 const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
 dotenv.config()
